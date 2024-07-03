06a0c81d80f24cb46df985b048bfb8fbc3d79f46
Daria Znamenskaya <forprojects99@gmail.com>
2024-07-03 23:57:44 +0300

    Homework 2

--- src/components/header/component.jsx
+++
@@ -1,9 +0,0 @@
-import "./header.css"
-
-export const Header = () => {
-    return (
-        <div className="header-container">
-            <h1>Restaurants</h1>
-        </div>
-    );
-};
--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import '../style/main.css'
-import { App } from './components/app/app'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-);
